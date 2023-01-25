a6a078c89a67dc5d7a8b7669d1d8fa25ff2156f5
PauloGN <paulorsantos.gnt@outlook.com>
2023-01-25 00:07:10 -0800

    Enemy layers and change color

--- Metroidvania/Assets/Scripts/EnemyHealthController.cs
+++ Metroidvania/Assets/Scripts/EnemyHealthController.cs
@@ -7,11 +7,15 @@ public class EnemyHealthController : MonoBehaviour
 
     [SerializeField] int totalHealth = 3;
     [SerializeField] GameObject deathFX;
+    [SerializeField] SpriteRenderer spriteRendererREF;
+    [SerializeField] Color[] colors;
+    private int colorsIndex = 2;
 
 
     public void TakeDamage(int dmg)
     {
         totalHealth-= dmg;
+        ChangeEnemyColor();
 
         if (totalHealth <=0)
         {
@@ -23,5 +27,20 @@ public class EnemyHealthController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    void ChangeEnemyColor()
+    {
+        spriteRendererREF = GetComponentInChildren<SpriteRenderer>();
+        if (spriteRendererREF != null && colors.Length >= colorsIndex)
+        {
+
+            if(totalHealth > 0 && totalHealth < colors.Length)
+            {
+                colorsIndex--;
+                spriteRendererREF.color = colors[colorsIndex];
+            }
+        }
+    }
+
 
 }
